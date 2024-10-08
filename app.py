9d9df876b333e3b467321da394a5e1401474c00b
mdominguezd <martin.dominguez.duran@gmail.com>
2024-10-08 11:58:34 +0200

    added start time

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ import os
 
 import streamlit as st
 import pandas as pd
-from streamlit_folium import folium_static
+from streamlit_folium import st_folium
 
 from backend import *
+
+# Set page configuration
+st.set_page_config(
+    page_title="Veluweloopen",   # Title that appears in the browser tab
+    page_icon=":runner:",                        # Custom emoji or image as the favicon
+    layout="wide",                         # Can be "centered" or "wide"
+    initial_sidebar_state="collapsed"       # Can be "expanded", "collapsed", or "auto"
+)
 
 # Streamlit app layout
 st.title('Veluweloopen :runner:')
@@ -18,8 +26,9 @@ if user_files:
     user_selection = st.selectbox('Select a user to view their GPX route:', user_files)
 
     run_start_time, gpx_files = show_runner_data(user_selection)
-    
-    st.markdown("## Running start time:" + run_start_time)
+
+    st.markdown("<h1 style='text-align: center'> &#128336 </h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: white;'>Running start time: " + run_start_time + "</h2>", unsafe_allow_html=True)
 
     # Load selected user's GPX route
     files = []
@@ -31,6 +40,6 @@ if user_files:
     gpx_map = create_map(files)
     
     # Display the map
-    folium_static(gpx_map)
+    st_folium(gpx_map, width = '100%')
 else:
     st.error("No GPX files found. Please add GPX files to the 'gpx/' folder.")
